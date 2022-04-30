0458c1aadc6bc3181256753208f6a0c779b4d176
Oraz Janov <Orazjanov11@gmail.com>
2022-04-30 16:05:09 +0300

    main updated

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import Handlers.Handlers as HD
 from aiogram.utils import executor
 from Handlers import StateWork as SG, StateCommand as SC, StateOpen as SO, StateFun as SF
 
-
+SG.register_Handler_StateWork(HD.dp)
+SC.register_Handler_Statecommand(HD.dp)
+SO.register_Handler_StateOpen(HD.dp)
+SF.register_Handler_FunCommand(HD.dp)
 HD.register_Handler_Client(HD.dp)
 
 executor.start(HD.dp, HD.on_startup())
