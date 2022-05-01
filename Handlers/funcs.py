d3223834bfbba3377f6ed4a46ff37dc663655e9e
Orazjan <45052100+Orazjan@users.noreply.github.com>
2022-05-01 21:06:43 +0300

    Add files via upload

---
+++ Handlers/funcs.py
@@ -0,0 +1,78 @@
+import logging 
+import os
+import random as rd
+import pyautogui as pag
+from Handlers import state, task_proc, handlers
+
+PATH = "D://Projects/PY/ForBot/notification/screens/"
+
+def loggings():
+    Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+    logging.basicConfig(filename = f"{PATH}/logfile.log",
+                    filemode = "w",
+                    format = Log_Format, 
+                    level = logging.ERROR)
+
+    logger = logging.getLogger()
+    logger.error(state.return_message(f"For check \n{handlers.get_proc()}"))
+    logger.info(state.return_message(f"This is log file {handlers.get_proc()}"))
+
+def read_and_send_logs():
+    try:
+        logfile = open(f'{PATH}/logfile.log')
+    except :
+        return "Ошибка нет файла"
+        
+    array =[]
+    for p in logfile:
+        array.append(p+"\n")
+
+    return task_proc.list_to_string(array)
+
+def cancel():
+    os.system("shutdown -a")
+
+
+def shutdown():
+    os.system(
+        "shutdown -s -t 20 -c \"Сервер будет выключен через 20 секунд. Сохраните свои документы!\"")
+
+
+def reboot():
+    os.system(
+        "shutdown -r -t 20 -c \"Этот компьютер будет перезагружен через 20 секунд.\"")
+
+
+def leave_session():
+    os.system("shutdown -l")
+
+
+def kill_wind():
+    pag.hotkey('ctrl', 'w' )
+
+
+def kill_process(text):
+    os.system(f"taskkill /f /im {text}")
+
+
+def kill_func():
+    os.system("taskkill /f /im python.exe")
+
+def write_text(text):
+    pag.typewrite(f"{text}", interval=0.25)
+
+
+def mouse_rand(func):
+    pag.FAILSAFE = False
+    a = rd.randint(200, 1080)   # x
+    b = rd.randint(300, 1080)   # y
+    pag.moveTo(a, b, int(func))
+
+
+def window_warning(func):
+    pag.alert(func)
+
+
+def screenshot():
+    pag.screenshot(f'{PATH}/ss.png')
+    
---
+++ Handlers/state.py
@@ -0,0 +1,98 @@
+import psutil
+from Handlers import funcs
+import Handlers.opens as op
+from datetime import datetime
+from Keyboardz.keyboard_fun import *
+from Handlers.task_proc import get_processes_running, list_to_string
+
+
+def return_time():
+    dt = datetime.now()
+    bt = dt.strftime("%H:%M:%S - %d.%m")
+    return bt
+
+
+def return_message(text):
+    return text + return_time()
+
+def work_komp(func):
+    if (func == "Покинуть систему"):
+        func = "Вы покинете систему через несколько секунд "
+        funcs.leave_session()
+        return return_message(func)
+
+    elif (func == "Перезагрузка"):
+        func = "Компьютер перезагрузится через 20 секунд "
+        funcs.reboot()
+        return return_message(func)
+
+    elif (func == "Завершение работы"):
+        func = "Компьютер выключится через 20 секунд "
+        funcs.shutdown()
+        return return_message(func)
+        
+    else:
+        return return_message('Неправильная команда. Попробуйте выбрать другую\n')
+
+def fun_segment(func):
+    if (func == "Напечатать"):
+        func ='Введите текст на англ\n'
+        return return_message(func)
+    
+    elif (func=="Рандом с мышкой"):
+        func ='Введите количество секунд:\n'
+        return return_message(f"{func}")
+
+    elif (func == "Вывод окна"):
+        return return_message("Введите текст или выберите из меню:\n")        
+
+    elif (func == "Скриншот экрана"):
+        return return_message("Скриншот готов ")
+
+    else:
+        return return_message('Неправильная команда. Попробуйте выбрать другую\n')
+
+def status_komp(func):
+    if (func == "Батарея"):
+        battery = psutil.sensors_battery()
+        percent = int(battery.percent)
+        if (battery.power_plugged == True):
+            text = "Заряд батареи: " + str(percent) + "\nЗаряжается "
+            return return_message(text)
+        else:
+            text = "Заряд батареи: " + str(percent) + "\nНе заряжается "
+            return return_message(text)
+            
+    elif (func == "Закрыть окно"):
+        funcs.kill_wind()
+        return return_message("Окно закрыто ")
+
+    elif (func == 'Открытые программы'):
+        lstp = get_processes_running()
+        array =[]
+        for p in lstp:
+            array.append(p+"\n")
+        func = f"Открытые программы\n\n{list_to_string(array)}\n\n"
+        return return_message(func)
+    
+    elif (func == "Логи"):
+        return return_message(funcs.read_and_send_logs())
+  
+    else:
+        return return_message('Неправильная команда. Попробуйте выбрать другую\n')
+
+
+def open_web(func):
+    if (func == "Vk"):
+        func = "Открывается ВК "
+        op.open_vk()
+
+    elif (func == "YouTube"):
+        func = "Открывается YouTube "
+        op.open_youtube()
+
+    elif (func == "Другой сайт"):
+        func = "Введите ссылку на сайт: "
+    
+    else:
+        return return_message('Неправильная команда. Попробуйте выбрать другую\n')
