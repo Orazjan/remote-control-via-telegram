e6a8a780172dc19017686ade1194afe42dc83c3c
Orazjan <orazjanov11@gmail.com>
2022-12-15 21:54:01 +0300

    added Utilities

---
+++ Utilities/task_proc.py
@@ -0,0 +1,21 @@
+import re
+
+from subprocess import check_output
+
+def get_processes_running():
+    """
+    Takes tasklist output and parses the table into a dict
+    """
+    tasks = check_output(['tasklist']).decode('cp866', 'ignore').split("\r\n")
+    p = []
+    for task in tasks:
+        m = re.match(b'(.*?)\\s+(\\d+)\\s+(\\w+)\\s+(\\w+)\\s+(.*?)\\s.*', task.encode())
+        if m is not None:
+            p.append(m.group(1).decode())
+    return(p)
+
+def list_to_string(text):
+    str1 = ""
+    for ele in text:
+        str1 += ele  
+    return str1
